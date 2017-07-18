63e32cdc7d11cc323745619f18a91a3a2d58ea01
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-07-18 16:57:42 -0400

    fin

--- main.js
+++ main.js
@@ -1,56 +1,22 @@
-URL = "https://crossorigin.me/http://www.recipepuppy.com/api/"
+let searchbar = document.querySelector("form")
 
-let ul = document.querySelector("ul")
-fetch(URL).then(response => response.json()).then(data => {
-  for (var i = 0; i < data.results.length; i++) {
-    let li = document.createElement("li")
+searchbar.addEventListener("submit", (event) => {
+  event.preventDefault()
+  let searchBox = document.getElementById("Recipe-Search")
+  URL = `https://crossorigin.me/http://www.recipepuppy.com/api/?q=${searchBox.value}`
+  let ul = document.querySelector("ul")
+  fetch(URL).then(response => response.json()).then(data => {
+    for (var i = 0; i < data.results.length; i++) {
+      let li = document.createElement("li")
 
-    // let img = document.createElement("img")
-    // img.src = data.results[i].thumbnail
-    // li.appendChild(img)
+      let a = document.createElement("a")
+      a.textContent = data.results[i].title
+      a.href = data.results[i].href
+      li.appendChild(a)
 
-    let a = document.createElement("a")
-    a.textContent = data.results[i].title
-    a.href = data.results[i].href
-    li.appendChild(a)
+      li.style.backgroundImage = `url(${data.results[i].thumbnail})`
 
-    li.style.backgroundImage = `url(${data.results[i].thumbnail})`
-
-    ul.appendChild(li)
-  }
+      ul.appendChild(li)
+    }
+  })
 })
-
-// ?q=`${user.word}`
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-///
-//
-//
-//
-//
-//
-//
